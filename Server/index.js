f8e90d338c06f854df5d449818e3000de4f1827a
Nico DeMilio <njdemilio@gmail.com>
2024-11-30 11:06:14 -0500

    add Cors

--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,13 @@ const userController = require('./controllers/users.js')
 
 const PORT = 3000
 //MiddleWare
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    next()
+})
+
 app.use(express.json())
 app.use(express.static(__dirname + "/dist"))
 
