40b0556aa05218eecf87b3b56d6d70556313711d
Nico DeMilio <njdemilio@gmail.com>
2024-12-09 18:32:48 -0500

    try again

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/Client/dist"));
 
 // Public routes
 app.get("/", (req, res, next) => {
