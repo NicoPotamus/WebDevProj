0f3d049b81690d8fb0299f2f95516a049c515c9a
Nico DeMilio <njdemilio@gmail.com>
2024-12-15 10:44:37 -0500

    serve client

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "../Client/dist"));
 
 // Public routes
 app.get("/", (req, res, next) => {
@@ -39,7 +39,7 @@ app.use("/api/v1/following", authenticateJWT, followingController);
 app.use("/api/v1/stats", authenticateJWT, statsController);
 
 app.get("*", (req, res, next) => {
-    res.sendFile(__dirname + "/dist/index.html");
+    res.sendFile(__dirname + "../Client/dist/index.html");
 });
 
 // Error Handling
