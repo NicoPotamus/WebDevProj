43f30fe4f4a1c60a3e66b38c79718017b0eebf84
Nico DeMilio <njdemilio@gmail.com>
2024-12-18 20:16:01 -0500

    update User photo

--- Client/src/components/PersonalInfo.vue
+++ Client/src/components/PersonalInfo.vue
@@ -9,6 +9,43 @@ const user = ref<User>(props.user)
 watch(() => props.user, (newUser) => {
   user.value = newUser
 })
+
+function handleFileUpload(event: Event) {
+  const file = (event.target as HTMLInputElement).files?.[0]
+  if (file) {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      const img = new Image()
+      img.onload = () => {
+        const canvas = document.createElement('canvas')
+        const ctx = canvas.getContext('2d')
+        const maxWidth = 300
+        const maxHeight = 300
+        let width = img.width
+        let height = img.height
+
+        if (width > height) {
+          if (width > maxWidth) {
+            height *= maxWidth / width
+            width = maxWidth
+          }
+        } else {
+          if (height > maxHeight) {
+            width *= maxHeight / height
+            height = maxHeight
+          }
+        }
+
+        canvas.width = width
+        canvas.height = height
+        ctx?.drawImage(img, 0, 0, width, height)
+        user.value.photo = canvas.toDataURL('image/jpeg')
+      }
+      img.src = e.target?.result as string
+    }
+    reader.readAsDataURL(file)
+  }
+}
 </script>
 
 <template>
@@ -63,9 +100,12 @@ watch(() => props.user, (newUser) => {
       </div>
       <div class="box">
         <div class="field">
-          <label class="label">UserPhoto</label>
+          <label class="label">User Photo</label>
           <div class="control">
-            <input class="input" type="text" placeholder=".JPG or PNG" v-model="user.photo" />
+            <input class="input" type="file" @change="handleFileUpload" />
+          </div>
+          <div v-if="user.photo">
+            <img :src="user.photo" alt="User Photo" style="max-width: 100%; height: auto;" />
           </div>
         </div>
       </div>
